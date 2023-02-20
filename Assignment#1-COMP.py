48258f146a24ff1af96faf2092f065126cc52fa3
jengrantt <125925797+jengrantt@users.noreply.github.com>
2023-02-20 15:37:16 -0600

    Add files via upload

---
+++ Assignment#1-COMP.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+Gabrielle Kilpatrick, Mollie Cox, and Jenna Grant
+Coding File for Assignment 1.1
+The game is played as follows:  
+• Roll a six-sided die. 
+• If you roll a 1, 2 or a 3, the game is over. 
+• If you roll a 4, 5, or 6, you win that many dollars ($4, $5, or $6), and then roll again. 
+• With each additional roll, you have the chance to win more money, or you might roll a 
+game-ending 1, 2, or 3, at which time the game is over, and you keep whatever winnings 
+you have accumulated. 
+Use the randint() function from Python's Random module to get a die roll result, specifically look 
+at the functions for integers. 
+Set  your  code  to  run  10,000  simulations  of  the  game.  There  is  no  user  input  needed. 
+Output the following statistics: 
+1. the probability that a game ends with a non-zero amount (in percentages), 
+2. average amount won (in currency format), and 
+3. the largest amount won (in currency format). 
+At the end of your code answer the following questions as a comment: 
+• How likely do you think it is to win at least $4? (Hint: you may argue with or without 
+using the simulation outputs.) 
+• Would you pay $3 for a chance to play this game? Why or why not? How about $4? 
+"""
+
